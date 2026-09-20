7e1d6bfe6365ab0f2adeec36668859f4934cb383
Ruslan <ruslan.skorobahatko@gmail.com>
2026-09-20 13:03:38 +0300

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://pixabay.com/api/';
-const API_KEY = import.meta.env.VITE_PIXABAY_API_KEY;
+const API_KEY = '57664861-998876c815f7f33043a4b1ff1';
 
 export async function getImagesByQuery(query, page) {
   const response = await axios.get(API_URL, {
